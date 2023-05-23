88d541a3afcf178d79678b5bc0b2d2d6fd2ae3e2
DJFancyAl <abluemleiv@gmail.com>
2023-05-22 20:16:39 -0400

    More setup work

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 // Dependencies
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 
-// Routes
+// Home Route
 app.get('/', (req, res) => {
-    res.json({message: "Home Page"})
+    res.status(200).json({message: "Home Page"})
 })
 
-
+// Wildcard Route
+app.get('*', (req, res) => {
+    res.status(404).json({error: 'error404'})
+})
 
 // Listener
 app.listen(3000, () => {
