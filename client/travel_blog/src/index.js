114e7bdd1b22c009150d437b201c828a32f012ae
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-05-24 20:04:14 -0400

    update

--- client/travel_blog/src/index.js
+++ client/travel_blog/src/index.js
@@ -1,41 +1,25 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
 // );
 
 // HAMBURGER FUNCTION
-function navSlide() {
-  const menubtn = document.querySelector(".menubtn");
-  const nav = document.querySelector(".navlinks");
-  const navLinks = document.querySelectorAll(".navlinks li");
+//function navSlide() {
+//  const menubtn = document.querySelector(".menubtn");
+//  const nav = document.querySelector(".navlinks");
+ // const navLinks = document.querySelectorAll(".navlinks li");
   
-  menubtn.addEventListener("click", () => {
-      //Toggle Nav
-      nav.classList.toggle("nav-active");
-      
-      //Animate Links
-      navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-              link.style.animation = ""
-          } else {
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-          }
-      });
-      //Burger Animation
-      menubtn.classList.toggle("toggle");
-  });
-  
-}
+  //}
 
-navSlide();
+//navSlide();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
