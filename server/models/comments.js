6503224470039d257f21de322f539125f9291e20
DJFancyAl <abluemleiv@gmail.com>
2023-06-01 21:05:14 -0400

    Complete merge.

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,13 +1,15 @@
+// Dependencies
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
-
-
+// Comment Schema
 const commentSchema = new Schema ({
-    author: {type: mongoose.Schema.Types.ObjectId, ref:'Author', required: true},
-    blog: {type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true},
-    body: {type: String},
-    date: {type : Date, default: Date.now}
+    author: {type: Schema.Types.ObjectId, ref:'Author', required: true},
+    blog: {type: Schema.Types.ObjectId, ref: 'Blog', required: true},
+    title: {type: String}, required: true,
+    body: String,
+    date: { type : Date, default: Date.now },
 });
 
+// Export
 module.exports = mongoose.model('Comment', commentSchema)
