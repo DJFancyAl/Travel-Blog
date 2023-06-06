c11be678580810c08af04c287e2712d909f12932
DJFancyAl <abluemleiv@gmail.com>
2023-06-06 14:51:09 -0400

    Trying to change allowed IP addresses.

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://djfancyal.github.io/Travel-Blog/");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, x-access-token, X-Requested-With, Content-Type, Accept, Authorization");
     next();
