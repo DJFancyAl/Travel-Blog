0bbdcfe2ff80dd6a272bf30adbea88d5324f9141
DJFancyAl <abluemleiv@gmail.com>
2023-06-06 14:36:24 -0400

    Cleaned up styling, and removed some errors.

--- client/travel_blog/src/Components/Pages/EditBlog.js
+++ client/travel_blog/src/Components/Pages/EditBlog.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { MdEditNote } from "react-icons/md";
+import GrowButton from '../GrowButton'
 
 function EditBlog() {    
     // State
     const {id} = useParams()
     const navigate = useNavigate()
-    const [editedBlog, setEditedBlog] = useState({})
+    const [editedBlog, setEditedBlog] = useState({title: '', pic: '', body: ''})
 
     // Handle Change
     const handleChange = (e) => {
@@ -31,8 +32,10 @@ function EditBlog() {
         },
         body: JSON.stringify(editedBlog)
         })
-        
-        navigate(`/blog/${editedBlog._id}`)
+        const data = await response.json()
+        if(data.message){
+            navigate(`/blog/${editedBlog._id}`)
+        }
     }
 
     // Fetch Blog
@@ -48,26 +51,26 @@ function EditBlog() {
 
     return (
         <Container>
-            <h1>Edit:</h1>
+            <h1>Edit Your Blog</h1>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="title">
-                <Form.Label>Blog Title</Form.Label>
-                <Form.Control type="text" placeholder="Your title..." onChange={handleChange} value={editedBlog.title} />
+                    <Form.Label>Blog Title</Form.Label>
+                    <Form.Control type="text" placeholder="Your title..." onChange={handleChange} value={editedBlog.title} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="pic">
-                <Form.Label>Image URL</Form.Label>
-                <Form.Control type="text" placeholder="Your photo..." onChange={handleChange} value={editedBlog.pic} />
+                    <Form.Label>Image URL</Form.Label>
+                    <Form.Control type="text" placeholder="Your photo..." onChange={handleChange} value={editedBlog.pic} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="body">
-                <Form.Label>Post Body</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder='Write your blog...' onChange={handleChange} value={editedBlog.body} />
+                    <Form.Label>Post Body</Form.Label>
+                    <Form.Control as="textarea" rows={5} placeholder='Write your blog...' onChange={handleChange} value={editedBlog.body} />
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
-                Submit
-                </Button>
+                <GrowButton variant='primary' type='submit' start='120px'>
+                    Edit Blog <MdEditNote className='mb-1' size={20} />
+                </GrowButton>
             </Form>
         </Container>
     )
